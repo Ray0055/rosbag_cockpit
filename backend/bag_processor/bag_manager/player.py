2e45e173ee7553fbf1e7898a3cf4145acb56db05
Rui.Hu <rui.hu@rennteam-stuttgart.de>
2025-04-19 01:33:27 +0200

    [fix] add read ahead queue size to play bag

--- backend/bag_processor/bag_manager/player.py
+++ backend/bag_processor/bag_manager/player.py
@@ -64,6 +64,9 @@ class RosbagPlayer:
             cmd.append("--topics")
             cmd.extend(topics)
 
+        cmd.append("--read-ahead-queue-size")
+        cmd.append("10000")
+
         # Create a shell command with source commands and the ros2 bag play command
         # TODO: If you want to run fastapi in docker, you need to create a image with
         # ros2 installed and comm_pkg installed
